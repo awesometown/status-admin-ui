4d9c111bd8c50fc3fb29a813273a1f14c9c67dec
James Pennell <jpennell@ea.com>
2015-12-03 09:23:24 -0500

    Moved incident list and detail components to the components directory

---
+++ app/components/incidents/detail.js
@@ -0,0 +1,100 @@
+import React from "react";
+
+import { Panel, Grid, Row, Col } from "react-bootstrap";
+import { Link } from "react-router";
+
+import moment from "moment";
+
+export default React.createClass({
+
+	propTypes: {
+		incident: React.PropTypes.object.isRequired,
+		servicesMap: React.PropTypes.object.isRequired
+	},
+
+	getDefaultProps: function() {
+		return {
+			incident: {},
+			servicesMap: {}
+		};
+	},
+
+	componentDidMount: function () { },
+
+	render: function () {
+
+		console.log("Incident: %O", this.props.incident);
+		console.log("Services: %O", this.props.servicesMap);
+
+		let map = this.props.servicesMap;
+		let affectedServices = this.props.incident.affectedServiceIds;
+
+		console.log("Map: %O", map);
+		console.log("Affected Services: %O", affectedServices);
+
+		let serviceNodes = affectedServices.map(serviceId =>
+			<li key={ serviceId }>{ map[serviceId] }</li>);
+
+		console.log("Service Nodes: %O", serviceNodes);
+
+		let lastMessage = this.props.incident.incidentUpdates[0].description;
+		console.log("Last Message: %s", lastMessage);
+
+		let servicesPanel = null;
+		if (affectedServices.length > 0) {
+			servicesPanel = (
+				<Panel style={{fontSize:"90%", padding:"0px", backgroundColor:"#EEEEEE"}}>
+					<h5 style={{marginTop:"0px"}}>Affected Services:</h5>
+					<ul style={{paddingLeft:"20px", marginTop:"5px"}}>
+						{ serviceNodes }
+					</ul>
+				</Panel>
+			);
+		}
+
+		console.log("Services Panel: %O", servicesPanel);
+
+		let fromNow = moment(this.props.incident.updatedAt).fromNow();
+		console.log("From Now: %s", fromNow);
+
+		let incidentId = this.props.incident.id;
+		console.log("Incident Id: %s", incidentId);
+
+		let incidentTitle = this.props.incident.title;
+		console.log("Incident Title: %s", incidentTitle);
+
+		let incidentServiceStatusId = this.props.incident.title;
+		console.log("Incident Service Status Id: %s", incidentServiceStatusId);
+
+		let incidentState = this.props.incident.state;
+		console.log("Incident State: %s", incidentState);
+
+		return (
+			<Panel>
+				<Grid fluid={true}>
+					<Row>
+						<Col md={12}>
+							<p style={{float: "right", align: "right"}}>Updated { fromNow }</p>
+
+							<h3 style={{marginTop: "0px"}}>
+								<Link to={ "/incidents/" + incidentId }>{ incidentTitle }</Link>
+							</h3>
+						</Col>
+					</Row>
+					<Row>
+						<Col md={8}>
+
+							<h4>{ incidentServiceStatusId }</h4>
+
+							<p><strong>{ incidentState }</strong> - { lastMessage }</p>
+						</Col>
+						<Col md={4}>
+							{ servicesPanel }
+						</Col>
+					</Row>
+				</Grid>
+			</Panel>
+		);
+	}
+
+});
--- app/incidents/list.js
+++ app/incidents/list.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { Link } from "react-router";
+
 import { LinkContainer } from "react-router-bootstrap";
-import {PageHeader, Panel, Button, Grid, Col, Row, Table} from "react-bootstrap";
+import { PageHeader, Button } from "react-bootstrap";
 import { statusClient } from "../globals";
-import moment from "moment";
+import IncidentList from "../components/incidents/list.js"
+
 
 export default React.createClass({
+
 	getInitialState: function () {
 		return {
 			incidents: [],
@@ -15,33 +17,41 @@ export default React.createClass({
 	},
 
 	componentDidMount: function () {
-		let incidentsPromise = statusClient.getActiveIncidentsByType("unplanned")
-			.then(result => {
+
+		let incidentsPromise = statusClient.getActiveIncidentsByType("unplanned").then(result => {
+
 				if (this.isMounted()) {
-					this.setState({incidents: result.data.data});
+					this.setState({ incidents: result.data.data });
 					console.log(result);
 				}
+
 			})
-			.catch(result => console.log(result));
-		let servicesPromise = statusClient.getServices()
-			.then(result => {
+			.catch(result => console.err(result));
+
+		let servicesPromise = statusClient.getServices().then(result => {
+
 				if (this.isMounted()) {
+
 					let servicesMap = {};
 					for (let service of result.data.data) {
 						servicesMap[service.id] = service.name;
 					}
-					this.setState({servicesMap: servicesMap});
+					this.setState({ servicesMap: servicesMap });
+
 				}
+
 			})
-			.catch(result => console.log(result));
-		Promise.all([incidentsPromise, servicesPromise]).then(values => this.setState({loaded: true}));
+			.catch(result => console.err(result));
+
+		Promise.all([incidentsPromise, servicesPromise]).then(values => this.setState({ loaded: true }));
+
 	},
 
 	render: function () {
 
 		let content;
 		if (this.state.loaded) {
-			content = <IncidentList incidents={this.state.incidents} servicesMap={this.state.servicesMap}/>
+			content = <IncidentList incidents={ this.state.incidents } servicesMap={ this.state.servicesMap }/>
 		} else {
 			content = null;
 		}
@@ -50,69 +60,8 @@ export default React.createClass({
 			<div id="content">
 				<LinkContainer to="/incidents/new"><Button className="button">New Incident</Button></LinkContainer>
 				<PageHeader>Incidents</PageHeader>
-				{content}
+				{ content }
 			</div>
-		);
-	}
-});
-
-var IncidentList = React.createClass({
-	render: function () {
-		let incidentNodes = this.props.incidents.map(incident => <Incident key={incident.id} incident={incident}
-																		   servicesMap={this.props.servicesMap}/>);
-		return (
-			<Row>
-				<Col mdOffset={1} md={10}>
-					{incidentNodes}
-				</Col>
-			</Row>
-		);
-	}
-});
-
-var Incident = React.createClass({
-	render: function () {
-		let map = this.props.servicesMap;
-		let serviceNodes = this.props.incident.affectedServiceIds.map(serviceId => <li
-			key={serviceId}>{map[serviceId]}</li>);
-		let lastMessage = this.props.incident.incidentUpdates[0].description;
-
-		let servicesPanel = null;
-		if (this.props.incident.affectedServiceIds.length > 0) {
-			servicesPanel = (
-				<Panel style={{fontSize:"90%", padding:"0px", backgroundColor:"#EEEEEE"}}>
-					<h5 style={{marginTop:"0px"}}>Affected Services:</h5>
-					<ul style={{paddingLeft:"20px", marginTop:"5px"}}>
-						{serviceNodes}
-					</ul>
-				</Panel>);
-		}
-
-		return (
-			<Panel>
-				<Grid fluid={true}>
-					<Row>
-						<Col md={12}>
-							<p style={{float: "right", align: "right"}}>
-								Updated {moment(this.props.incident.updatedAt).fromNow()}</p>
-
-							<h3 style={{marginTop: "0px"}}><Link
-								to={"/incidents/"+this.props.incident.id}>{this.props.incident.title}</Link></h3>
-						</Col>
-					</Row>
-					<Row>
-						<Col md={8}>
-
-							<h4>{this.props.incident.serviceStatusId}</h4>
-
-							<p><strong>{this.props.incident.state}</strong> - {lastMessage}</p>
-						</Col>
-						<Col md={4}>
-							{servicesPanel}
-						</Col>
-					</Row>
-				</Grid>
-			</Panel>
 		);
 	}
 });
